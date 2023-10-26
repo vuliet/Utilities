270d2d2985bae2c1fda01b5862f97ba3f1995853
ThaiVu <thai35.tv@gmail.com>
2023-10-26 09:24:13 +0700

    create compareV2Utils

--- Utilities/Utilities/CompareUtils.cs
+++ Utilities/Utilities/CompareUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection;
 
 namespace Utilities.Utilities
 {
@@ -11,6 +12,56 @@ namespace Utilities.Utilities
             if (y == null) return 1;
             var compare = CompareInfo.GetCompareInfo("en-US");
             return compare.Compare(x, y, CompareOptions.Ordinal);
+        }
+    }
+
+    public class CompareV2Utils
+    {
+        public static List<string> CompareObjects<T>(T obj1, T obj2)
+        {
+            List<string> differences = new List<string>();
+
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                object value1 = property.GetValue(obj1);
+                object value2 = property.GetValue(obj2);
+
+                if (property.GetValue(value2) is null)
+                    continue;
+
+                if (!Equals(value1, value2))
+                    differences.Add(property.Name);
+            }
+
+            return differences;
+        }
+
+        public static List<string> CompareAndUpdateObjects<T>(T obj1, T obj2)
+        {
+            List<string> differences = new List<string>();
+
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                object value1 = property.GetValue(obj1);
+                object value2 = property.GetValue(obj2);
+
+                if (property.GetValue(value2) is null)
+                    continue;
+
+                if (!Equals(value1, value2))
+                {
+                    differences.Add(property.Name);
+                    property.SetValue(obj1, value2);
+                }
+            }
+
+            return differences;
         }
     }
 }
